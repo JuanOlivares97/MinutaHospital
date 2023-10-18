b7bad701a4a06e4c04c3f9bb9122b884a629f801
CataLazo <cat.lazo@duocuc.cl>
2023-10-17 21:52:08 -0300

    + cambios a las vistas

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const session = require('express-session');
-
-
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
@@ -9,17 +7,25 @@ const corsOptions = {
   origin: 'https://lowedev.cl'
 };
 app.use(cors(corsOptions));
+
 const connection = require("./database/database.js");
 const LoginRoutes = require('./routes/LoginRoutes.js')
 
-app.set("view engine", "ejs");
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
-});
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'secreto', resave: false, saveUninitialized: false }));
+app.use(express.static('public'))
 
-app.use('/login', LoginRoutes);
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+  res.render('/loginview')
+});
+
+app.get('/', (req, res) => {
+  res.redirect('/auth')
+});
+
+app.use('/auth', LoginRoutes);
 
 app.use("/resources", express.static("public"));
 app.use("/resources", express.static(__dirname + "/public"));
--- routes/LoginRoutes.js
+++ routes/LoginRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const session = require('express-session');
 const bcrypt = require('bcrypt');
-const db = require("./database/database.js");
+const db = require("../database/database.js");
 
 const Route = express.Router();
 
-Route.get('/login', (req, res) => {
+Route.get('/', (req, res) => {
     res.send(`
-      <form method="post" action="/login">
+      <form method="post" action="/auth/login">
         <input type="text" name="username" placeholder="Nombre de usuario" required><br>
         <input type="password" name="password" placeholder="Contraseña" required><br>
         <button type="submit">Iniciar Sesión</button>
@@ -16,19 +17,28 @@ Route.get('/login', (req, res) => {
 
 Route.post('/login', (req, res) => {
     const { username, password } = req.body;
-    db.query('SELECT * FROM usuarios WHERE username = ?', [username], (err, results) => {
+    db.query('SELECT * FROM Funcionario WHERE CONCAT(`RutFuncionario`,'-',`DvFuncionario`) = ?', [username], (err, results) => {
       if (err) throw err;
       if (results.length === 1) {
         const user = results[0];
-        bcrypt.compare(password, user.password, (err, isMatch) => {
+        console.log(user.contrasena)
+        bcrypt.compare(password, user.contrasena, (err, isMatch) => {
           if (err) throw err;
   
           if (isMatch) {
             // Autenticación exitosa, redirige en función del tipo de usuario
-            if (user.userType === '') {
-              res.redirect('/admin');
-            } else {
-              res.redirect('/user');
+            if (user.IdTipoFuncionario === 1) {
+              res.redirect('/Nutricionista');
+            }else if(user.IdTipoFuncionario === 2) {
+              res.redirect('/NutricionistaJefe');
+            }else if(user.IdTipoFuncionario === 3){
+              res.redirect('/Tecnico'); 
+            }else if(user.IdTipoFuncionario === 4){
+              res.redirect('/Clinico');
+            }else if(user.IdTipoFuncionario === 5){
+              res.redirect('/Recursos');
+            }else if(user.IdTipoFuncionario === 6){
+              res.redirect('/Recaudacion');
             }
           } else {
             res.send('Credenciales incorrectas');
