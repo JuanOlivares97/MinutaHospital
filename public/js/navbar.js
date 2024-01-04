07cd657e310ca6dfa154702918de482bf2259564
CataLazo <cat.lazo@duocuc.cl>
2024-01-03 23:05:07 -0300

    +css + logo en sidebar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,44 +1,28 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const showNavbar = (toggleId, navId, bodyId, headerId) => {
-    const toggle = document.getElementById(toggleId),
-      nav = document.getElementById(navId),
-      bodypd = document.getElementById(bodyId),
-      headerpd = document.getElementById(headerId)
+const showNavbar = (toggleId, navId, bodyId, headerId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId)
 
-    // Validate that all variables exist
-    if (toggle && nav && bodypd && headerpd) {
-      toggle.addEventListener('click', () => {
-        // show navbar
-        nav.classList.toggle('show')
-        // change icon
-        toggle.classList.toggle('bx-x')
-        // add padding to body
-        bodypd.classList.toggle('body-pd')
-        // add padding to header
-        headerpd.classList.toggle('body-pd')
-      })
-    }
+  // Validate that all variables exist
+  if (toggle && nav && bodypd && headerpd) {
+    // Añadir un elemento de imagen al inicio de la barra de navegación
+    const logoImg = document.createElement('img');
+    logoImg.src = '/resources/img/logopequeno-hospital.png'; // Reemplaza 'ruta_del_logo.png' con la ruta real de tu logo
+    logoImg.alt = 'Logo'; // Agrega un texto alternativo para accesibilidad
+    nav.insertBefore(logoImg, nav.firstChild);
+
+    toggle.addEventListener('click', () => {
+      // show navbar
+      nav.classList.toggle('show');
+      // change icon
+      toggle.classList.toggle('bx-x');
+      // add padding to body
+      bodypd.classList.toggle('body-pd');
+      // add padding to header
+      headerpd.classList.toggle('body-pd');
+    });
   }
+}
 
-  showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
-
-  /*===== LINK ACTIVE =====*/
-  const linkColor = document.querySelectorAll('.nav_link')
-
-  function colorLink() {
-    if (linkColor) {
-      linkColor.forEach(l => l.classList.remove('active'))
-      this.classList.add('active')
-    }
-  }
-  linkColor.forEach(l => l.addEventListener('click', colorLink))
-
-});
-
-document.getElementById('resoruces/img/logopequeno.png').addEventListener('click', function() {
-  changeImage('inactive');
-});
-
-document.getElementById('resoruces/img/logogrande.png').addEventListener('click', function() {
-  changeImage('active');
-});
+showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
